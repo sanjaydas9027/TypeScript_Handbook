5877049c691e7dc69fed35eaa5f0cc9e7ce81c7c
sanjaydas9027 <sanjaydas9027@gmail.com>
2024-06-08 16:58:51 +0530

    added files

---
+++ Day2/ReadMe.md
@@ -0,0 +1,6 @@
+# Type, Type Inference, Type Annotations in TypeScript
+
+- Typescript is a statically typed language
+- Type Inference num1 --> Compile time--> adds type at compile time based on data given
+- type Annotation --> num:number
+- 
---
+++ Day2/lang.ts
@@ -0,0 +1,16 @@
+const user = {
+    firstName: 'Sanjay',
+    age: 26
+}
+
+console.log(user.firstName);
+
+let age: number;
+
+age = 30;
+// age="gekk";
+
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+}
+console.log(sum(10, 20));
